73c5aba1a241ea0ee02ec04580a43b6368914252
moboa <mbajifope@gmail.com>
2017-08-20 22:31:23 -0400

    hw1 - build 37 key synthesizer

---
+++ hw1/GuitarHero.java
@@ -0,0 +1,43 @@
+import stdlib.StdAudio;
+import stdlib.StdDraw;
+import synthesizer.GuitarString;
+
+/**
+ * A client that uses the synthesizer package to create a a 37 key synthesizer
+ * @author moboa
+ */
+public class GuitarHero {
+    public static void main(String[] args) {
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        GuitarString[] strings = new GuitarString[keyboard.length()];
+
+        for (int i = 0; i < keyboard.length(); i++) {
+            double frequency = 440 * Math.pow(2, (i - 24) / 12);
+            strings[i] = new GuitarString(frequency);
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int indexOfGuitarString = keyboard.indexOf(key);
+                strings[indexOfGuitarString].pluck();
+            }
+
+        /* compute the superposition of samples */
+            double sample = 0;
+            for (GuitarString string : strings) {
+                sample += string.sample();
+            }
+
+        /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+        /* advance the simulation of each guitar string by one step */
+            for (GuitarString string : strings) {
+                string.tic();
+            }
+        }
+    }
+}
